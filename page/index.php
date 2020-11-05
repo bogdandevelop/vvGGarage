dc70d42afcf5a7d72ae06435a39a249a77f395c2
bogdandevelop <61016442+bogdandevelop@users.noreply.github.com>
2020-11-05 21:17:16 +0200

    Add files via upload

---
+++ page/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" type ="text/css" href="css/style.css">
+</head>
+<body>
+    <div id="header">Шапка</div>
+    <div id="leftcol">Левая колонка</div>
+    <div id="content">Центр</div>
+</body>
+</html>
